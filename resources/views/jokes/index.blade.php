3d1d1829efbd48eee13f758f74d8419ffeee598c
AlessandroVano <alessandrovanoli.16@gmail.com>
2022-01-31 09:27:18 +0100

    add @if avviso cancellazione

--- resources/views/jokes/index.blade.php
+++ resources/views/jokes/index.blade.php
@@ -3,7 +3,15 @@
 @section('content')
   <div class="container">
       <h1 class="mb-5">DI COLMI NE ABBIAMO?</h1>
-      
+
+      @if (session('deleted'))
+      <div class="alert alert-success">
+        <strong>{{ session('deleted') }}</strong>
+        Hai cancellato il colmo correttamente senza nessun intoppo!!
+      </div>
+        
+      @endif
+
       <div class="row">
        
           @foreach ($jokes as $joke)
@@ -13,14 +21,17 @@
               <div class="card-body"></div>
             <h4 class="card-title">{{ $joke->title }}</h3>
          {{--    <p class="card-text">{{ $joke->body }}</p>  --}}
-            <a  href="{{ route('jokes.show', $joke->id) }}" class="btn btn-primary mb-3">clicca per scoprire</a>
+         <div>
+           <a  href="{{ route('jokes.show', $joke->id) }}" class="btn btn-primary mb-3">clicca per scoprire</a>
 
-            <a  href="{{ route('jokes.edit', $joke->id) }}" class="btn btn-primary mb-3">clicca per modificare</a>
-            <form action="{{ route('jokes.destroy', $joke->id) }}" method="POST">
-            @csrf {{-- runniamo il form grazie a questo comando --}}
-            @method('DELETE') {{-- diamogli il metodo per attivare la cancellazione --}}
-            <input type="submit" class="btn btn-danger" value="delete" onclick="return confirm('Sicuro di voler eliminare questo colmo?')">
-            </form>
+           <a  href="{{ route('jokes.edit', $joke->id) }}" class="btn btn-primary mb-3">clicca per modificare</a>
+           <form class="mb-2" action="{{ route('jokes.destroy', $joke->id) }}" method="POST">
+           @csrf {{-- runniamo il form grazie a questo comando --}}
+           @method('DELETE') {{-- diamogli il metodo per attivare la cancellazione --}}
+           <input type="submit" class="btn btn-danger" value="delete" onclick="return confirm('Sicuro di voler eliminare questo colmo?')">
+           </form>
+
+         </div>
           </div>
         
              
